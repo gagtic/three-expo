49b492808e7cdb59e4a6ffa5f0f4fd054e433e85
gagtic <gagtic@hotmail.com>
2023-11-25 03:14:05 +0500

    init

--- App.tsx
+++ App.tsx
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Suspense } from "react";
+import { Canvas, MeshProps, useFrame, useLoader } from "@react-three/fiber";
+import { useState, useRef } from "react";
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
 
-export default function App() {
+const Box = (props: any) => {
+  const [active, setActive] = useState<boolean>(false);
+  const mesh = useRef<MeshProps>();
+
+  useFrame((state, delta) => {
+    if (active && mesh.current) {
+      mesh.current.rotation.y -= delta;
+      mesh.current.rotation.x += delta;
+    }
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? 1.5 : 1}
+      onClick={() => setActive(!active)}
+    >
+      <boxGeometry />
+      <meshStandardMaterial color={active ? "green" : "grey"} />
+    </mesh>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Shoe = (props: any) => {
+  console.log("yeet", require("./assets/Airmax/shoe.obj"));
+  const obj = useLoader(OBJLoader, require("./assets/Airmax/shoe.obj"));
+
+  return (
+    <mesh>
+      <primitive object={obj} scale={10} />
+    </mesh>
+  );
+};
+
+const App = () => {
+  return (
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[20, 20, 20]} decay={0} intensity={Math.PI} />
+
+      <Suspense fallback={null}>
+        <Shoe />
+      </Suspense>
+    </Canvas>
+  );
+};
+
+export default App;
